3b3ba326f6adba2c2ad1ac365d7837a8dc854622
emilio13 <thesorcerer13@gmail.com>
2021-03-21 12:07:07 +0100

    first phases of ci/cd configuration

---
+++ test/test.js
@@ -0,0 +1,8 @@
+    const request = require("supertest");
+    const app = require("../index");
+
+    describe("GET /", () => {
+      it("respond with message", (done) => {
+        request(app).get("/").expect("CI/CD Node.js Tutorial for Deloitte", done);
+      })
+    });
